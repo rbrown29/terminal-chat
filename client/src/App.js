1afeb9768be8ce66bac9b30133b34f5c7af8da53
rbrown29 <53094729+rbrown29@users.noreply.github.com>
2023-04-23 12:03:22 -0700

    commit

---
+++ client/src/App.js
@@ -0,0 +1,49 @@
+import "./App.css";
+import io from "socket.io-client";
+import { useState } from "react";
+import Chat from "./Chat";
+
+const socket = io.connect("http://localhost:3001");
+
+function App() {
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
+
+  const joinRoom = () => {
+    if (username !== "" && room !== "") {
+      socket.emit("join_room", room);
+      setShowChat(true);
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1 className="typing-effect">Welcome to Terminal Chat</h1>
+      {!showChat ? (
+        <div className="joinChatContainer">
+          <input
+            type="text"
+            placeholder="UserName..."
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Room ID..."
+            onChange={(event) => {
+              setRoom(event.target.value);
+            }}
+          />
+          <button onClick={joinRoom}>Join A Room</button>
+        </div>
+      ) : (
+        <Chat socket={socket} username={username} room={room} />
+      )}
+    </div>
+  );
+}
+
+export default App;
+
